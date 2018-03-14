dac9bcda4bd3b870bd30545c82f076f4e5a02130
Chris Liebman <chris.l@taboola.com>
2018-03-14 16:02:55 -0700

    added comments to Serial example

--- examples/DLogSerial/DLogSerial.ino
+++ examples/DLogSerial/DLogSerial.ino
@@ -9,6 +9,12 @@ DLog& dlog = DLog::getLog();
 static const char* sTAG = "setup";
 static const char* lTAG = "loop";
 
+//
+// We will use this to add a timestamp to the start of each log line.
+//
+// While we dont use level it could be used send character sequences
+// to change the color of lines based on level
+//
 void pre_log(DLogBuffer& buffer, DLogLevel level)
 {
     (void)level; // we know its not used
@@ -18,9 +24,26 @@ void pre_log(DLogBuffer& buffer, DLogLevel level)
 void setup()
 {
     Serial.begin(115200);
+
+    //
+    // The default log level is info.  Here we change the log level for
+    // 'loop' tagged lines to debug.
+    //
     dlog.setLevel(lTAG, DLOG_LEVEL_DEBUG);
+
+    //
+    // Here we add a "pre" function to add a timestamp at the start of each line
+    //
     dlog.setPreFunc(pre_log);
+
+    //
+    // Start the logger with Serial as output.  This could be any object implementing "Print"
+    //
     dlog.begin(new DLogPrintWriter(Serial));
+
+    //
+    // Start logging!
+    //
     dlog.info(sTAG, "Startup!");
     dlog.debug(sTAG, "this wont show because 'setup' level not set to at least DEBUG");
     dlog.info(sTAG, F("Done"));
